3725a65e1588f1527a211ad42a47da46d6b6e783
Maximiliano Fierro <elmasse@gmail.com>
2015-06-06 20:09:35 -0300

    tests refactor

--- test/integration/Lexer.js
+++ test/integration/Lexer.js
@@ -2,36 +2,145 @@
 
 var chai   = require('chai');
 var Lexer  = require('../../lib/Lexer');
-var Token  = require('../helper/Token');
+var path   = require('path');
 
 var expect = chai.expect;
 
 describe('Lexer integration tests', function(){
 
-    describe('Lexer describing grammar programmatically', function(){
-        describe('Lexer with IDENTIFIER NUMERIC and EOF rules', function(){
-            var lexer,
-                tokens = [],
-                init;
+    describe('Lexer with simple grammar', function() {
+        var Token  = require('../helper/simple/Token');
 
-            lexer = Lexer.create();
+        describe('Lexer describing grammar programmatically', function () {
+            describe('Lexer with IDENTIFIER NUMERIC and EOF rules', function(){
+                var lexer,
+                    tokens = [],
+                    init;
 
-            init = lexer.initializer();
+                lexer = Lexer.create();
 
-            //define expressions
-            init ('{digit}')    (/[0-9]/);
-            init ('{number}')   (/{digit}+(\.{digit}+)?(E[+-]?{digit}+)?/);
-            //define state rules
-            init (/[A-Za-z_]+/) (function(/*match*/){ tokens.push(Token.IDENTIFIER); });
-            init (/{number}/)   (function(/*match*/){ this.installToken(Token.NUMERIC); });
-            init (/[ \s\n\t]+/) (/*No action*/);
-            init (/$/)          (function(/*match*/){ tokens.push(Token.EOF); return false;});
+                init = lexer.initializer();
 
-            //define auxiliar methods
-            init ('installToken') (function(value){tokens.push(value);});
+                //define expressions
+                init ('{digit}')    (/[0-9]/);
+                init ('{number}')   (/{digit}+(\.{digit}+)?(E[+-]?{digit}+)?/);
+                //define state rules
+                init (/[A-Za-z_]+/) (function(/*match*/){ tokens.push(Token.IDENTIFIER); });
+                init (/{number}/)   (function(/*match*/){ this.installToken(Token.NUMERIC); });
+                init (/[ \s\n\t]+/) (/*No action*/);
+                init (/$/)          (function(/*match*/){ tokens.push(Token.EOF); return false;});
+
+                //define auxiliar methods
+                init ('installToken') (function(value){tokens.push(value);});
 
 
-            describe('using lex method', function(){
+                describe('using lex method', function(){
+                    it('lex(source) should parse ID NUMERIC ID EOF', function(){
+                        var source = 'identifier 0.3E+99 another_ID';
+
+                        tokens = [];
+
+                        lexer.lex(source);
+
+                        expect(tokens).to.have.length(4);
+                        expect(tokens).to.have.deep.property('[0]', Token.IDENTIFIER);
+                        expect(tokens).to.have.deep.property('[1]', Token.NUMERIC);
+                        expect(tokens).to.have.deep.property('[2]', Token.IDENTIFIER);
+                        expect(tokens).to.have.deep.property('[3]', Token.EOF);
+
+                    });
+
+                    it('lex(source) should parse ID NUMERIC ID EOF with escaped symbols', function(){
+                        var source = 'identifier\t999\n  another_ID \n';
+
+                        tokens = [];
+
+                        lexer.lex(source);
+                        expect(tokens).to.have.length(4);
+                        expect(tokens).to.have.deep.property('[0]', Token.IDENTIFIER);
+                        expect(tokens).to.have.deep.property('[1]', Token.NUMERIC);
+                        expect(tokens).to.have.deep.property('[2]', Token.IDENTIFIER);
+                        expect(tokens).to.have.deep.property('[3]', Token.EOF);
+
+                    });
+
+                    it('lex() should parse ID NUMERIC ID EOF using setSource()', function(){
+                        var source = 'identifier 999 another_ID';
+
+                        tokens = [];
+
+                        lexer.setSource(source);
+
+                        lexer.lex();
+
+                        expect(tokens).to.have.length(4);
+                        expect(tokens).to.have.deep.property('[0]', Token.IDENTIFIER);
+                        expect(tokens).to.have.deep.property('[1]', Token.NUMERIC);
+                        expect(tokens).to.have.deep.property('[2]', Token.IDENTIFIER);
+                        expect(tokens).to.have.deep.property('[3]', Token.EOF);
+
+                    });
+                });
+
+
+                describe('using next method and setSource', function(){
+
+                    it('next() should parse ID NUMERIC ID EOF one at a time', function(){
+                        var source = 'identifier 999 another_ID';
+
+                        tokens = [];
+
+                        lexer.setSource(source);
+
+                        lexer.next();
+                        expect(tokens).to.have.length(1);
+                        expect(tokens).to.have.deep.property('[0]', Token.IDENTIFIER);
+
+                        lexer.next();
+                        expect(tokens).to.have.length(2);
+                        expect(tokens).to.have.deep.property('[1]', Token.NUMERIC);
+
+                        lexer.next();
+                        expect(tokens).to.have.length(3);
+                        expect(tokens).to.have.deep.property('[2]', Token.IDENTIFIER);
+
+                        lexer.next();
+                        expect(tokens).to.have.length(4);
+                        expect(tokens).to.have.deep.property('[3]', Token.EOF);
+
+                    });
+                });
+
+            });
+        });
+        describe('Lexer with grammar from json', function () {
+            var Grammar = {
+                "expressions": {
+                    '{digit}' : '[0-9]',
+                    '{number}':  '{digit}+(\\.{digit}+)?(E[+-]?{digit}+)?'
+                },
+                "rules": {
+                    '[A-Za-z_]+'    : 'Token.IDENTIFIER|install',
+                    '{number}'      : 'Token.NUMERIC|install',
+                    '[ \\s\\n\\t]+' : '', /*NO ACTION*/
+                    '$'             : 'Token.EOF|install|END'
+                }
+            };
+
+            describe('Lexer with IDENTIFIER NUMERIC and EOF rules', function(){
+                var lexer,
+                    tokens = [];
+
+                lexer = Lexer.create({
+                    grammar: Grammar,
+                    helpers: {
+                        Token: Token,
+                        install: function(token){
+                            tokens.push(token);
+                        }
+                    }
+                });
+
                 it('lex(source) should parse ID NUMERIC ID EOF', function(){
                     var source = 'identifier 0.3E+99 another_ID';
 
@@ -47,28 +156,31 @@ describe('Lexer integration tests', function(){
 
                 });
 
-                it('lex(source) should parse ID NUMERIC ID EOF with escaped symbols', function(){
-                    var source = 'identifier\t999\n  another_ID \n';
+            });       
+        });
+        describe('Lexer with grammar from file', function () {
+            var SIMPLE_GRAMMAR_JSON = path.resolve(__dirname,'..', 'helper/simple/Grammar.json');
+
+            describe('Lexer with IDENTIFIER NUMERIC and EOF rules', function () {
+                var lexer,
+                    tokens = [];
+
+                lexer = Lexer.create({
+                    json: SIMPLE_GRAMMAR_JSON,
+                    helpers: {
+                        Token: Token,
+                        install: function(token){
+                            tokens.push(token);
+                        }
+                    }
+                });
+
+                it('lex(source) should parse ID NUMERIC ID EOF', function(){
+                    var source = 'identifier 0.3E+99 another_ID';
 
                     tokens = [];
 
                     lexer.lex(source);
-                    expect(tokens).to.have.length(4);
-                    expect(tokens).to.have.deep.property('[0]', Token.IDENTIFIER);
-                    expect(tokens).to.have.deep.property('[1]', Token.NUMERIC);
-                    expect(tokens).to.have.deep.property('[2]', Token.IDENTIFIER);
-                    expect(tokens).to.have.deep.property('[3]', Token.EOF);
-
-                });
-
-                it('lex() should parse ID NUMERIC ID EOF using setSource()', function(){
-                    var source = 'identifier 999 another_ID';
-
-                    tokens = [];
-
-                    lexer.setSource(source);
-
-                    lexer.lex();
 
                     expect(tokens).to.have.length(4);
                     expect(tokens).to.have.deep.property('[0]', Token.IDENTIFIER);
@@ -77,115 +189,9 @@ describe('Lexer integration tests', function(){
                     expect(tokens).to.have.deep.property('[3]', Token.EOF);
 
                 });
-            });
 
-
-            describe('using next method and setSource', function(){
-
-                it('next() should parse ID NUMERIC ID EOF one at a time', function(){
-                    var source = 'identifier 999 another_ID';
-
-                    tokens = [];
-
-                    lexer.setSource(source);
-
-                    lexer.next();
-                    expect(tokens).to.have.length(1);
-                    expect(tokens).to.have.deep.property('[0]', Token.IDENTIFIER);
-
-                    lexer.next();
-                    expect(tokens).to.have.length(2);
-                    expect(tokens).to.have.deep.property('[1]', Token.NUMERIC);
-
-                    lexer.next();
-                    expect(tokens).to.have.length(3);
-                    expect(tokens).to.have.deep.property('[2]', Token.IDENTIFIER);
-
-                    lexer.next();
-                    expect(tokens).to.have.length(4);
-                    expect(tokens).to.have.deep.property('[3]', Token.EOF);
-
-                });
-            });
-
+            });       
         });
     });
-    describe('Lexer with grammar from json', function(){
-        var Grammar = {
-            "expressions": {
-                '{digit}' : '[0-9]',
-                '{number}':  '{digit}+(\\.{digit}+)?(E[+-]?{digit}+)?'
-            },
-            "rules": {
-                '[A-Za-z_]+'    : 'Token.IDENTIFIER|install',
-                '{number}'      : 'Token.NUMERIC|install',
-                '[ \\s\\n\\t]+' : '', /*NO ACTION*/
-                '$'             : 'Token.EOF|install|END'
-            }
-        };
 
-        describe('Lexer with IDENTIFIER NUMERIC and EOF rules', function(){
-            var lexer,
-                tokens = [];
-
-            lexer = Lexer.create({
-                grammar: Grammar,
-                helpers: {
-                    Token: Token,
-                    install: function(token){
-                        tokens.push(token);
-                    }
-                }
-            });
-
-            it('lex(source) should parse ID NUMERIC ID EOF', function(){
-                var source = 'identifier 0.3E+99 another_ID';
-
-                tokens = [];
-
-                lexer.lex(source);
-
-                expect(tokens).to.have.length(4);
-                expect(tokens).to.have.deep.property('[0]', Token.IDENTIFIER);
-                expect(tokens).to.have.deep.property('[1]', Token.NUMERIC);
-                expect(tokens).to.have.deep.property('[2]', Token.IDENTIFIER);
-                expect(tokens).to.have.deep.property('[3]', Token.EOF);
-
-            });
-
-        });       
-    });
-
-    describe('Lexer with grammar from file', function(){
-        describe('Lexer with IDENTIFIER NUMERIC and EOF rules', function(){
-            var lexer,
-                tokens = [];
-
-            lexer = Lexer.create({
-                json: __dirname+'/Grammar.json',
-                helpers: {
-                    Token: Token,
-                    install: function(token){
-                        tokens.push(token);
-                    }
-                }
-            });
-
-            it('lex(source) should parse ID NUMERIC ID EOF', function(){
-                var source = 'identifier 0.3E+99 another_ID';
-
-                tokens = [];
-
-                lexer.lex(source);
-
-                expect(tokens).to.have.length(4);
-                expect(tokens).to.have.deep.property('[0]', Token.IDENTIFIER);
-                expect(tokens).to.have.deep.property('[1]', Token.NUMERIC);
-                expect(tokens).to.have.deep.property('[2]', Token.IDENTIFIER);
-                expect(tokens).to.have.deep.property('[3]', Token.EOF);
-
-            });
-
-        });       
-    });
 });
